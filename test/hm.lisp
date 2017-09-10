e76c902d8cb46fa923cabd63c84bcf7f8a63c2ba
tsdye <tsd@tsdye.com>
2017-09-10 02:08:00 -1000

    Finished tests for lookup-option and transitive reduction.  All pass.

--- test/hm.lisp
+++ test/hm.lisp
@@ -10,35 +10,47 @@
 (defsuite test)
 (in-suite test)
 
-(defvar *digraph* nil
+(defvar *transitive* nil
+  "Variable for use in hm tests.")
+
+(defvar *intransitive* nil
   "Variable for use in hm tests.")
 
 (defvar *cfg* nil
   "Variable for use in hm tests.")
 
 (defixture transitive-graph
-  (:setup (setf *digraph* (graph:populate (make-instance 'graph:digraph)
-                                          :nodes '(a b c)
-                                          :edges '((a b)
-                                                   (b c)
-                                                   (a c)))
-                *cfg* (make-default-configuration)))
-  (:teardown (setf *digraph* nil
+  (:setup (setf *transitive* (graph:populate (make-instance 'graph:digraph)
+                                             :nodes '(a b c)
+                                             :edges '((a b)
+                                                      (b c)
+                                                      (a c)))
+                *intransitive* (graph:populate (make-instance 'graph:digraph)
+                                               :nodes '(a b c)
+                                               :edges '((a b)
+                                                        (b c)))
+                *cfg* (hm:default-configuration)))
+  (:teardown (setf *transitive* nil
+                   *intransitive* nil
                    *cfg* nil)))
 
-(defixture default-configuration
-  (:setup (setf *cfg* (make-default-configuration)))
+(defixture default-config
+  (:setup (setf *cfg* (hm:default-configuration)))
+  (:teardown (setf *cfg* nil)))
+
+(defixture empty-config
+  (:setup (setf *cfg* (hm:empty-configuration)))
   (:teardown (setf *cfg* nil)))
 
 ;;; Tests for internal functions
 
 (deftest fast-matrix-test ()
-  (with-fixture default-configuration
-    (is (hm::fast-matrix-p *cfg*))
+  (with-fixture default-config
+    (is (hm:fast-matrix-p *cfg*))
     (hm::set-option *cfg* "General configuration" "fast-matrix" "off")
-    (is (not (hm::fast-matrix-p *cfg*)))
+    (is (not (hm:fast-matrix-p *cfg*)))
     (hm::set-option *cfg* "General configuration" "fast-matrix" "foo")
-    (is (not (hm::fast-matrix-p *cfg*)))))
+    (is (not (hm:fast-matrix-p *cfg*)))))
 
 (deftest quotes-around-test ()
   (let ((goal "\"abc\""))
@@ -55,41 +67,45 @@
   (is (string= (hm::graphviz-edge-style 6) "dotted"))
   (is (string= (hm::graphviz-edge-style 7) "bold")))
 
+(deftest lookup-option-test ()
+  (with-fixture default-config
+    (let ((master (hm:master-table))
+          (result t))
+      (dolist (row master)
+        (and result
+             (or
+              (string= (nth 6 row)
+                       (hm:lookup-option *cfg* (nth 1 row) (nth 2 row)
+                                         (nth 3 row) (nth 4 row) (nth 5 row)))
+              (setf result nil))))
+      (is result))))
+
 (deftest correlated-node-test ()
   (with-fixture transitive-graph
-    (is (eq (hm::correlated-node (first (graph:nodes *digraph*))
-                                 (second (graph:nodes *digraph*))
+    (is (eq (hm::correlated-node (first (graph:nodes *transitive*))
+                                 (second (graph:nodes *transitive*))
                                  nil)
             'a=b))
-    (is (string= (hm::correlated-node (first (graph:nodes *digraph*))
-                                 (second (graph:nodes *digraph*))
+    (is (string= (hm::correlated-node (first (graph:nodes *transitive*))
+                                 (second (graph:nodes *transitive*))
                                  t)
             "A=B"))))
 
 (deftest node-index-test ()
   (with-fixture transitive-graph
-    (let ((i (hm::make-node-index *digraph*)))
-      (is (eq (first (graph:nodes *digraph*))
+    (let ((i (hm::make-node-index *transitive*)))
+      (is (eq (first (graph:nodes *transitive*))
               (fset:lookup i 0)))
-      (is (eq (second (graph:nodes *digraph*))
+      (is (eq (second (graph:nodes *transitive*))
               (fset:lookup i 1)))
-      (is (eq (third (graph:nodes *digraph*))
+      (is (eq (third (graph:nodes *transitive*))
               (fset:lookup i 2))))))
-
-(deftest transitive-reduction-new ()
-  (with-fixture transitive-graph
-    (is (tree-equal (graph:edges (hm::transitive-reduction-new
-                           *digraph*
-                           *cfg*))
-                     '((a b) (b c))))))
 
 (deftest transitive-reduction ()
   (with-fixture transitive-graph
-    (is (tree-equal (graph:edges (hm::transitive-reduction
-                                  *digraph*
-                                  *cfg*
-                                  nil))
-                    '((a b) (b c))))))
+    (is (graph:graph-equal
+         (hm::transitive-reduction *transitive*)
+         *intransitive*))))
 
 (deftest solarized-map-name ()
   (is (string-equal "002b36" (hm::solarized-map "base03"))))
@@ -103,7 +119,7 @@
 (deftest read-write-configuration ()
   (let ((path-name "../test/temp/test-config.ini")
         (config-from-file nil))
-    (with-fixture default-configuration
+    (with-fixture default-config
       (write-configuration *cfg* path-name)
       (setf config-from-file (read-configuration-from-files nil path-name))
       (is (equal (get-configuration-sections *cfg*)
@@ -116,7 +132,7 @@
   (let ((general-path-name "../test/temp/test-general-config.ini")
         (graphviz-path-name "../test/temp/test-graphviz-config.ini")
         (config-from-file nil))
-    (with-fixture default-configuration
+    (with-fixture default-config
       (write-general-configuration *cfg* general-path-name)
       (write-Graphviz-style-configuration *cfg* graphviz-path-name)
       (setf config-from-file
@@ -129,8 +145,8 @@
 
 ;; test reset-option
 (deftest test-reset-option ()
-  (let ((default-config (make-default-configuration)))
-    (with-fixture default-configuration
+  (let ((default-config (hm:default-configuration)))
+    (with-fixture default-config
       (is (equal (get-all-configuration-options *cfg*)
                  (get-all-configuration-options default-config)))
       (reset-option default-config "General configuration" "legend" "on")
@@ -140,7 +156,7 @@
 ;; test set-input-file
 (deftest test-set-input-file ()
   (let ((file-name "../test/temp/contexts-eg.csv"))
-    (with-fixture default-configuration
+    (with-fixture default-config
       (set-input-file *cfg* "contexts" file-name t)
       (set-input-file *cfg* "observations" file-name nil)
       (is (string= (hm::get-option *cfg* "Input files" "contexts") file-name))
@@ -153,7 +169,7 @@
 
 ;; test set-dot-file
 (deftest test-set-dot-file ()
-  (with-fixture default-configuration
+  (with-fixture default-config
     (set-dot-file *cfg* "chronology-dot" "foo.dot" nil)
     (set-dot-file *cfg* "sequence-dot" "bar.dot" nil)
     (is (string= (hm::get-option *cfg* "Output files" "chronology-dot") "foo.dot"))
