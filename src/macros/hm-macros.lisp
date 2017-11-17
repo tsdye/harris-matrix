3e067453bfbb4816da21467aff3c1ae95179a736
tsdye <tsd@tsdye.com>
2017-11-16 15:40:45 -1000

    Rearranged asset and resource files

---
+++ src/macros/hm-macros.lisp
@@ -0,0 +1,33 @@
+;;; hm-macros.lisp
+
+;; Copyright (C) Thomas Dye 2017
+
+;; Licensed under the Gnu Public License Version 3 or later
+
+(in-package #:hm)
+(named-readtables:in-readtable lol:lol-syntax)
+
+;; Macros
+
+(defmacro <-dot (seq element dot-attr)
+  `(to-dot-macro (,seq ,element ,dot-attr)))
+
+;; From On Lisp, p. 92, a macro for testing macroexpansion
+(defmacro mac (expr)
+  `(pprint (macroexpand-1 ',expr)))
+
+;; threading macro
+;; http://www.teknoids.net/content/immutable-persistent-data-structures-common-lisp
+
+(defmacro -> (x &optional (form nil form-supplied-p) &rest more)
+  (if form-supplied-p
+      (if more
+          `(-> (-> ,x ,form)
+               ,@more)
+          (if (listp form)
+              `(,(car form)
+                ,x
+                ,@
+                (cdr form))
+              (list form x)))
+      x))
