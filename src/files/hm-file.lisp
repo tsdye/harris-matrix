d658eeb6cd3b297cbce608a877c8741fe6509a1d
tsdye <tsd@tsdye.com>
2017-11-18 15:47:21 -1000

    Changes for project-directory

--- src/files/hm-file.lisp
+++ src/files/hm-file.lisp
@@ -6,6 +6,8 @@
 
 (in-package #:hm)
 
+;; color paths
+
 (defun cet-pathname (name)
   "Returns a path to the CET .csv file NAME."
   (let ((source (asdf:system-source-directory :hm))
@@ -24,8 +26,10 @@
         (full-name (uiop:merge-pathnames* "resources/brewer/" name)))
     (uiop:merge-pathnames* full-name source)))
 
+;; input file paths
+
 (defun configuration-pathname (name)
-  "Returns a path to the configuration .ini file NAME."
+  "Returns a path to the system configuration .ini file NAME."
   (let ((source (asdf:system-source-directory :hm))
         (full-name (uiop:merge-pathnames* "resources/configurations/" name)))
     (uiop:merge-pathnames* full-name source)))
@@ -45,17 +49,6 @@ give notice."
     (error "Unable to read ~a.~&" name)))
 
 ;; API
-(defun write-levels (graph out-file)
-  (let ((levels (graph:levels graph)))
-    (with-open-file (stream out-file :direction :output
-                                     :if-exists :overwrite
-                                     :if-does-not-exist :create)
-      (maphash (lambda (key value)
-                 (cl-csv:write-csv-row (list key value)
-                                       :stream stream))
-               levels))))
-
-;; API
 (defun write-default-configuration (path-name)
   "Write the default configuration to path-name."
   (let ((config (make-default-or-empty-configuration (master-table))))
@@ -63,9 +56,9 @@ give notice."
       (write-stream config stream))))
 
 ;; API
-(defun write-configuration (cfg path-name)
-  "Write configuration, CFG, to the file, PATH-NAME."
-  (with-open-file (stream path-name :direction :output :if-exists :supersede)
+(defun write-configuration (cfg file-name)
+  "Write configuration, CFG, to the file, FILE-NAME."
+  (with-open-file (stream file-name :direction :output :if-exists :supersede)
     (write-stream cfg stream)))
 
 ;; API
@@ -87,3 +80,41 @@ give notice."
       (when (Graphviz-section-p section)
         (remove-section cfg section)))
     (write-configuration cfg path-name)))
+
+(defun read-configuration-from-files (verbose &rest file-names)
+  "Reads the initialization files FILE-NAMES and returns a configuration. Errors
+out if one or more initialization files were not read. Prints a status message."
+  (let ((config (make-default-or-empty-configuration (master-table))))
+    (dolist (file file-names)
+      (when (null (probe-file file))
+        (error "Error: unable to read file: ~s.~&" file)))
+    (when verbose
+      (format t "Read ~r initialization file~:p: ~{~a~^, ~}.~&"
+              (length file-names) file-names))
+    (dolist (file file-names)
+      (read-files config (list file)))
+    config))
+
+;; output files
+;; API
+
+(defun write-levels (graph out-file cfg)
+  "Write levels of GRAPH to OUT-FILE in the project directory specified in CFG."
+  (let ((levels (graph:levels graph))
+        (out-file-path (uiop:merge-pathnames* out-file (get-project-directory cfg))))
+    (with-open-file (stream out-file-path :direction :output
+                                          :if-exists :overwrite
+                                          :if-does-not-exist :create)
+      (maphash (lambda (key value)
+                 (cl-csv:write-csv-row (list key value)
+                                       :stream stream))
+               levels))))
+
+(defun get-project-directory (cfg &optional verbose)
+  "Check if the user's project directory exists, if so, return a path to it. If
+not, return a path to the default project directory."
+  (let ((user (project-directory cfg)))
+    (if user (probe-file user)
+        (uiop:merge-pathnames*
+         "resources/default-project/"
+          (asdf:system-source-directory :hm)))))
