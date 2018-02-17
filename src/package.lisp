09d4865c26724794b3e9f84f4f1a1f0b3984e40e
Thomas S. Dye <tsd@tsdye.com>
2018-02-16 17:33:40 -1000

    Clean up list of exported functions

--- src/package.lisp
+++ src/package.lisp
@@ -8,16 +8,16 @@
    :alexandria
    :inferior-shell)
   (:export
+   :classifiable-attributes
+   :classifiers
    :write-default-configuration
    :write-configuration
-   :write-Graphviz-style-configuration
-   :write-general-configuration
    :reset-option
    :set-input-file
    :set-output-file
-   :read-configuration-from-files
    :show-configuration-options
    :show-configuration-sections
+   :read-configuration-from-files
    :run-project
    :run-project/example
    :write-classifier))
